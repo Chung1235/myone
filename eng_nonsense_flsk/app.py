243fa393575154bb99781152b63ae579512a9716
Chung1235 <yellowice5005@gmail.com>
2025-02-08 21:01:24 +0900

    Update app.py

--- eng_nonsense_flsk/app.py
+++ eng_nonsense_flsk/app.py
@@ -5,16 +5,16 @@ import random
 app = Flask(__name__)
 
 questions = [
-    {"question": "What comes once in a minute, twice in a moment, but never in a thousand years?", "answer": "m"},
-    {"question": "The more you take, the more you leave behind. What am I?", "answer": "footsteps"},
-    {"question": "What has keys but can't open locks?", "answer": "piano"},
-    {"question": "What word is spelled incorrectly in every dictionary?", "answer": "incorrectly"},
-    {"question": "What is always coming, but never arrives?", "answer": "tomorrow"},
-    {"question": "What is it that if you have, you want to share me, and if you share, you do not have?", "answer": "secret"},
-    {"question": "What is it that goes up, but never comes down?", "answer": "age"},
-    {"question": "What has hands but doesn't clap?", "answer": "clock"},
-    {"question": "What begins with an E but only has one letter?", "answer": "envelope"},
-    {"question": "What can you catch but not throw?", "answer": "cold"}
+    {"question": "What comes once in a minute, twice in a moment, but never in a thousand years?", "answer": ["m"]},
+    {"question": "The more you take, the more you leave behind. What am I?", "answer": ["footsteps"]},
+    {"question": "What has keys but can't open locks?", "answer": ["piano", "keyboard"]},
+    {"question": "What word is spelled incorrectly in every dictionary?", "answer": ["incorrectly"]},
+    {"question": "What is always coming, but never arrives?", "answer": ["tomorrow"]},
+    {"question": "What is it that if you have, you want to share me, and if you share, you do not have?", "answer": ["secret"]},
+    {"question": "What is it that goes up, but never comes down?", "answer": ["age"]},
+    {"question": "What has hands but doesn't clap?", "answer": ["clock", "watch"]},
+    {"question": "What begins with an E but only has one letter?", "answer": ["envelope"]},
+    {"question": "What can you catch but not throw?", "answer": ["cold"]}
 ]
 random.shuffle(questions)
 
